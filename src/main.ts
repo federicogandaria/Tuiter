6a38eaddfe159a31e295046d17ad00914244310f
federicogandaria <federicogandaria@gmail.com>
2023-02-25 12:23:01 -0300

    validation

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
+      whitelist: true, //Hace que solo se agreguen las propiedades que estan en el DTO
     }),
   );
   await app.listen(3000);
