6150023784b0bd43321c54017c0f89b74d600e5a
federicogandaria <federicogandaria@gmail.com>
2023-02-25 12:24:54 -0300

    otro pipe en main seguridad

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,8 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
-      whitelist: true, //Hace que solo se agreguen las propiedades que estan en el DTO
+      whitelist: true, //Hace que solo se agreguen las propiedades que estan en el DTO, buena practica
+      forbidNonWhitelisted: true, //TIRA ERROR 400 SI PASAMOS PROPIEDAD QUE NO VA EN POSTMAN
     }),
   );
   await app.listen(3000);
